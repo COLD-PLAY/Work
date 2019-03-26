7b4210b9f27c04ec0b35c911397280c45728cc33
COLD-PLAY <mi_xi_shi_zi@163.com>
2019-03-26 14:30:46 +0800

    做了两道三个数之和类型的题

--- LeetCode.py
+++ LeetCode.py
@@ -1,360 +1,398 @@
 # 3. Longest Substring Without Repeating Characters
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        max = 0
-        res_str = ''
-        length = len(s)
-        for i in range(length):     
-            if s[i] not in res_str:
-                res_str += s[i]
-                if len(res_str) > max:
-                    max = len(res_str)
-            else:
-                pos = res_str.index(s[i])
-                res_str = res_str[pos+1:] + s[i]
+	def lengthOfLongestSubstring(self, s: str) -> int:
+		max = 0
+		res_str = ''
+		length = len(s)
+		for i in range(length):	 
+			if s[i] not in res_str:
+				res_str += s[i]
+				if len(res_str) > max:
+					max = len(res_str)
+			else:
+				pos = res_str.index(s[i])
+				res_str = res_str[pos+1:] + s[i]
 
-        return max
+		return max
 
 # 4. Median of Two Sorted Arrays
 class Solution:
-    def findMedianSortedArrays(self, nums1, nums2) -> float:
-        l3 = nums1 + nums2
-        l3.sort()
-        return l3[len(l3) // 2] if len(l3) % 2 == 1 else (l3[len(l3) // 2 - 1] + l3[len(l3) // 2]) / 2
+	def findMedianSortedArrays(self, nums1, nums2) -> float:
+		l3 = nums1 + nums2
+		l3.sort()
+		return l3[len(l3) // 2] if len(l3) % 2 == 1 else (l3[len(l3) // 2 - 1] + l3[len(l3) // 2]) / 2
 
 # 7. Reverse Integer
 class Solution:
-    def reverse(self, x: int) -> int:
-        flag = 1 if x > 0 else -1
-        x = x if x > 0 else -x
-        x = int(str(x)[::-1])
-        if x > (pow(2, 31) - 1) or x < -pow(2, 31):
-            return 0
-        return flag*x
+	def reverse(self, x: int) -> int:
+		flag = 1 if x > 0 else -1
+		x = x if x > 0 else -x
+		x = int(str(x)[::-1])
+		if x > (pow(2, 31) - 1) or x < -pow(2, 31):
+			return 0
+		return flag*x
 
 # 9. Palindrome Number
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if str(x)[::-1] == str(x):
-            return True
-        return False
+	def isPalindrome(self, x: int) -> bool:
+		if str(x)[::-1] == str(x):
+			return True
+		return False
 
 # 13. Roman to Integer
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        special = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
-        normal = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        res = 0
-        for ch in special.keys():
-            if ch in s:
-                s.replace(ch, "")
-                res += special[ch]
-        for ch in s:
-            res += normal[ch]
-        return res
+	def romanToInt(self, s: str) -> int:
+		special = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+		normal = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+		res = 0
+		for ch in special.keys():
+			if ch in s:
+				s.replace(ch, "")
+				res += special[ch]
+		for ch in s:
+			res += normal[ch]
+		return res
 
 # 14. Longest Common Prefix
 class Solution:
-    def longestCommonPrefix(self, strs) -> str:
-        res = ""
-        str_num = len(strs)
-        if str_num == 0:
-            return res
-        length = [len(str) for str in strs]
-        min_len = min(length)
-        for i in range(min_len):
-            for j in range(str_num - 1):
-                if strs[j][i] != strs[j + 1][i]:
-                    return res
-            res += strs[0][i]
-        return res
+	def longestCommonPrefix(self, strs) -> str:
+		res = ""
+		str_num = len(strs)
+		if str_num == 0:
+			return res
+		length = [len(str) for str in strs]
+		min_len = min(length)
+		for i in range(min_len):
+			for j in range(str_num - 1):
+				if strs[j][i] != strs[j + 1][i]:
+					return res
+			res += strs[0][i]
+		return res
 
 # 14. Longest Common Prefix, set niubility
 class Solution:
-    def longestCommonPrefix(self, strs) -> str:
-        res = ""
-        strs = zip(*strs)
-        for x in strs:
-            sx = set(x)
-            if len(sx) == 1:
-                res += list(sx)[0]
-            else:
-                return res
-        return res
+	def longestCommonPrefix(self, strs) -> str:
+		res = ""
+		strs = zip(*strs)
+		for x in strs:
+			sx = set(x)
+			if len(sx) == 1:
+				res += list(sx)[0]
+			else:
+				return res
+		return res
 
 # 20. Valid Parentheses
 class Solution:
-    def isValid(self, s: str) -> bool:
-        left = ['(', '[', '{']
-        right = [')', ']', '}']
-        stack = []
-        for ch in s:
-            if ch in left:
-                stack.append(ch)
-            else:
-                if len(stack) == 0 or stack[-1] != left[right.index(ch)]:
-                    return False
-                stack.pop()
+	def isValid(self, s: str) -> bool:
+		left = ['(', '[', '{']
+		right = [')', ']', '}']
+		stack = []
+		for ch in s:
+			if ch in left:
+				stack.append(ch)
+			else:
+				if len(stack) == 0 or stack[-1] != left[right.index(ch)]:
+					return False
+				stack.pop()
 
-        if len(stack) == 0:
-            return True
-        return False
+		if len(stack) == 0:
+			return True
+		return False
 
 # 20. Valid Parentheses, easy to understand
 class Solution:
-    def isValid(self, s: str) -> bool:
-        length = len(s)
-        while (length):
-            s = s.replace('()', '')
-            s = s.replace('[]', '')
-            s = s.replace('{}', '')
-            if len(s) == length:
-                return False
-            length = len(s)
-        return True
+	def isValid(self, s: str) -> bool:
+		length = len(s)
+		while (length):
+			s = s.replace('()', '')
+			s = s.replace('[]', '')
+			s = s.replace('{}', '')
+			if len(s) == length:
+				return False
+			length = len(s)
+		return True
 
 # 21. Merge Two Sorted Lists
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+	def __init__(self, x):
+		self.val = x
+		self.next = None
 
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2e) -> ListNode:
-        if l1 is None: return l2
-        if l2 is None: return l1
-        if l1 is None and l2 is None: return None
+	def mergeTwoLists(self, l1: ListNode, l2e) -> ListNode:
+		if l1 is None: return l2
+		if l2 is None: return l1
+		if l1 is None and l2 is None: return None
 
-        head = ListNode(0)
-        cur = head
+		head = ListNode(0)
+		cur = head
 
-        while l1 and l2:
-            if l1.val < l2.val:
-                cur.next = l1
-                l1 = l1.next
-            else:
-                cur.next = l2
-                l2 = l2.next
-            cur = cur.next
+		while l1 and l2:
+			if l1.val < l2.val:
+				cur.next = l1
+				l1 = l1.next
+			else:
+				cur.next = l2
+				l2 = l2.next
+			cur = cur.next
 
-        cur.next = l1 or l2
-        return head.next
+		cur.next = l1 or l2
+		return head.next
 
 # 26. Remove Duplicates from Sorted Array
 class Solution:
-    def removeDuplicates(self, nums) -> int:
-        length = 0
-        for num in nums:
-            i = 0
-            while i < length:
-                if nums[i] == num:
-                    break
-                i += 1
-            if i == length:
-                nums[length] = num
-                length += 1
+	def removeDuplicates(self, nums) -> int:
+		length = 0
+		for num in nums:
+			i = 0
+			while i < length:
+				if nums[i] == num:
+					break
+				i += 1
+			if i == length:
+				nums[length] = num
+				length += 1
 
-        return length
-
-# 26. Remove Duplicates from Sorted Array 
-# !!!!!!! NOTE:SORTED
-class Solution:
-    def removeDuplicates(self, nums) -> int:
-        sets = list(set(nums))
-        sets.sort()
-        length = len(sets)
-        for i in range(length):
-            nums[i] = sets[i]
-        return length
+		return length
 
 # 26. Remove Duplicates from Sorted Array 
 # !!!!!!! NOTE:SORTED
 class Solution:
-    def removeDuplicates(self, nums) -> int:
-        if len(nums) <= 1:
-            return len(nums)
-        length = 0
-        for i in range(1, len(nums)):
-            if nums[i] != nums[length]:
-                length += 1
-                nums[length] = nums[i]
-        return length + 1
+	def removeDuplicates(self, nums) -> int:
+		sets = list(set(nums))
+		sets.sort()
+		length = len(sets)
+		for i in range(length):
+			nums[i] = sets[i]
+		return length
+
+# 26. Remove Duplicates from Sorted Array 
+# !!!!!!! NOTE:SORTED
+class Solution:
+	def removeDuplicates(self, nums) -> int:
+		if len(nums) <= 1:
+			return len(nums)
+		length = 0
+		for i in range(1, len(nums)):
+			if nums[i] != nums[length]:
+				length += 1
+				nums[length] = nums[i]
+		return length + 1
 
 
 # 27. Remove Element
 class Solution:
-    def removeElement(self, nums, val: int) -> int:
-        length = len(nums)
-        k = 0
-        for i in range(length):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-        return k
+	def removeElement(self, nums, val: int) -> int:
+		length = len(nums)
+		k = 0
+		for i in range(length):
+			if nums[i] != val:
+				nums[k] = nums[i]
+				k += 1
+		return k
 
 # 5. Longest Palindromic Substring
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        length = len(s)
-        l = length
-        if length <= 1: return s
-        while l > 1:
-            for i in range(length - l + 1):
-                if s[i:i+l] == s[i:i+l][::-1]:
-                    return s[i:i+l]
-            l -= 1
-        return s[0]
+	def longestPalindrome(self, s: str) -> str:
+		length = len(s)
+		l = length
+		if length <= 1: return s
+		while l > 1:
+			for i in range(length - l + 1):
+				if s[i:i+l] == s[i:i+l][::-1]:
+					return s[i:i+l]
+			l -= 1
+		return s[0]
 
 # 28. Implement strStr()
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        length_haystack = len(haystack)
-        length_needle = len(needle)
-        if length_needle > length_haystack: return -1
-        for i in range(length_haystack - length_needle + 1):
-            if haystack[i:length_needle + i] == needle:
-                return i
-        return -1
+	def strStr(self, haystack: str, needle: str) -> int:
+		length_haystack = len(haystack)
+		length_needle = len(needle)
+		if length_needle > length_haystack: return -1
+		for i in range(length_haystack - length_needle + 1):
+			if haystack[i:length_needle + i] == needle:
+				return i
+		return -1
 
 # 6. ZigZag Conversion
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        length = len(s)
-        res = ''
-        if length <= numRows or numRows == 1:
-            return s
-        nums = []
-        for i in range(1, numRows):
-            nums.append((numRows - i) * 2)
-        nums.append((numRows - 1) * 2)
+	def convert(self, s: str, numRows: int) -> str:
+		length = len(s)
+		res = ''
+		if length <= numRows or numRows == 1:
+			return s
+		nums = []
+		for i in range(1, numRows):
+			nums.append((numRows - i) * 2)
+		nums.append((numRows - 1) * 2)
 
-        for i in range(numRows):
-            j = i
-            flag = 0
-            while j < length:
-                res += s[j]
-                if nums[i] == (numRows - 1) * 2:
-                    j += nums[i]
-                elif flag:
-                    j += (numRows - 1) * 2 - nums[i]
-                    flag = 0
-                else:
-                    j += nums[i]
-                    flag = 1
-        return res
+		for i in range(numRows):
+			j = i
+			flag = 0
+			while j < length:
+				res += s[j]
+				if nums[i] == (numRows - 1) * 2:
+					j += nums[i]
+				elif flag:
+					j += (numRows - 1) * 2 - nums[i]
+					flag = 0
+				else:
+					j += nums[i]
+					flag = 1
+		return res
 
 # 6. ZigZag Conversion # niubility
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1 or len(s) <= numRows:
-            return s
+	def convert(self, s: str, numRows: int) -> str:
+		if numRows == 1 or len(s) <= numRows:
+			return s
 
-        L = [''] * numRows
-        index, step = 0, 1
+		L = [''] * numRows
+		index, step = 0, 1
 
-        for ch in s:
-            L[index] += ch
-            if index == 0:
-                step = 1
-            elif index == numRows - 1:
-                step = -1
-            index += step
+		for ch in s:
+			L[index] += ch
+			if index == 0:
+				step = 1
+			elif index == numRows - 1:
+				step = -1
+			index += step
 
-        return ''.join(L)
+		return ''.join(L)
 
 # 35. Search Insert Position
 class Solution:
-    def searchInsert(self, nums, target: int) -> int:
-        if len(nums) == 0 or nums[len(nums) - 1] < target:
-            return len(nums)
-        for i in range(len(nums)):
-            if nums[i] >= target:
-                return i
+	def searchInsert(self, nums, target: int) -> int:
+		if len(nums) == 0 or nums[len(nums) - 1] < target:
+			return len(nums)
+		for i in range(len(nums)):
+			if nums[i] >= target:
+				return i
 
 # 8. String to Integer (atoi)
 class Solution:
-    def myAtoi(self, str: str) -> int:
-        match = re.match(r'^ *([+-]{0,1}\d+)', str)
-        if not match:
-            return 0
-        n = int(match[1])
+	def myAtoi(self, str: str) -> int:
+		match = re.match(r'^ *([+-]{0,1}\d+)', str)
+		if not match:
+			return 0
+		n = int(match[1])
 
-        if n < -(2 ** 31): return -(2 ** 31)
-        if n >= 2 ** 31: return 2 ** 31 - 1
-        return n
+		if n < -(2 ** 31): return -(2 ** 31)
+		if n >= 2 ** 31: return 2 ** 31 - 1
+		return n
 
 # 53. Maximum Subarray
 class Solution:
-    def maxSubArray(self, nums) -> int:
-        for i in range(1, len(nums)):
-            if nums[i - 1] > 0:
-                nums[i] += nums[i - 1]
-        return max(nums)
+	def maxSubArray(self, nums) -> int:
+		for i in range(1, len(nums)):
+			if nums[i - 1] > 0:
+				nums[i] += nums[i - 1]
+		return max(nums)
 
 # 11. Container With Most Water
 class Solution:
-    def maxArea(self, height) -> int:
-        Max = -1
-        l = 0
-        r = len(height) - 1
-        while l < r:
-            Max = max(Max, min(height[r], height[l]) * (r - l))
-            if height[l] < height[r]:
-                l += 1
-            else:
-                r -= 1
-        return Max
+	def maxArea(self, height) -> int:
+		Max = -1
+		l = 0
+		r = len(height) - 1
+		while l < r:
+			Max = max(Max, min(height[r], height[l]) * (r - l))
+			if height[l] < height[r]:
+				l += 1
+			else:
+				r -= 1
+		return Max
 
 # 12. Integer to Roman
 class Solution:
-    def intToRoman(self, num: int) -> str:
-        value = {
-            0: ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
-            1: ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
-            2: ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
-            3: ['', 'M', 'MM', 'MMM']
-        }
-        i = 0
-        res = ''
-        while num:
-            res = value[i][num % 10] + res
-            num //= 10
-            i += 1
+	def intToRoman(self, num: int) -> str:
+		value = {
+			0: ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+			1: ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+			2: ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
+			3: ['', 'M', 'MM', 'MMM']
+		}
+		i = 0
+		res = ''
+		while num:
+			res = value[i][num % 10] + res
+			num //= 10
+			i += 1
 
-        return res
+		return res
 
 # 58. Length of Last Word
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        import re
-        res = re.findall(r'\w+', s)
-        return len(res[-1]) if len(res) > 0 else 0
+	def lengthOfLastWord(self, s: str) -> int:
+		import re
+		res = re.findall(r'\w+', s)
+		return len(res[-1]) if len(res) > 0 else 0
 
 # 66. Plus One 65.75%
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-    	num = 0
-    	for i in range(len(digits)):
-    		num = num * 10 + digits[i]
-    	num += 1
-    	res = []
-    	while num:
-    		res.insert(0, num % 10)
-    		num = num // 10
-    	return res
+	def plusOne(self, digits: List[int]) -> List[int]:
+		num = 0
+		for i in range(len(digits)):
+			num = num * 10 + digits[i]
+		num += 1
+		res = []
+		while num:
+			res.insert(0, num % 10)
+			num = num // 10
+		return res
 
 # 70. Climbing Stairs 67.05%
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        res = [1, 2]
-        i = n
-        while i > 2:
-        	res.append(res[-1] + res[-2])
-        	i -= 1
-        return res[n - 1]
+	def climbStairs(self, n: int) -> int:
+		res = [1, 2]
+		i = n
+		while i > 2:
+			res.append(res[-1] + res[-2])
+			i -= 1
+		return res[n - 1]
 
-# 15. 3Sum
+# 15. 3Sum 借鉴的别人的很优秀的方法，牛皮 89.81%
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+	def threeSum(self, nums: List[int]) -> List[List[int]]:
+		res = []
+		nums.sort()
+		length = len(nums)
+		for i in range(length - 2):
+			if nums[i] > 0: break
+			if i > 0 and nums[i] == nums[i - 1]: continue
+			l, r = i + 1, length - 1
+			while l < r:
+				total = nums[i] + nums[l] + nums[r]
+				if total > 0:
+					r -= 1
+				elif total < 0:
+					l += 1
+				else:
+					res.append([nums[i], nums[l], nums[r]])
+					while l < r and nums[l] == nums[l + 1]:
+						l += 1
+					while l < r and nums[r] == nums[r - 1]:
+						r -= 1
+					l += 1
+					r -= 1
+		return res
+
+# 16. 3Sum Closest 78.09%
+class Solution:
+	def threeSumClosest(self, nums: List[int], target: int) -> int:
+		res = nums[0] + nums[1] + nums[2] - target
+		nums.sort()
+		length = len(nums)
+		for i in range(length - 2):
+			if i > 0 and nums[i] == nums[i - 1]: continue
+			l, r = i + 1, length - 1
+			while l < r:
+				diff = nums[i] + nums[l] + nums[r] - target
+				if diff == 0: return target
+				if abs(res) > abs(diff): res = diff
+				elif diff > 0: r -= 1
+				else: l += 1
+		return res + target
