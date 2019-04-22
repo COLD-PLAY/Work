8bebbb66f5889ecc960d23cee095f4f1f0661f38
COLD-PLAY <liaozhou98@gmail.com>
2019-04-22 16:00:09 +0800

    两道智商检测题

--- LeetCode.py
+++ LeetCode.py
@@ -1524,3 +1524,63 @@ class Solution(object): # 38.18%
 		:rtype: List[int]
 		"""
 		return sorted([x ** 2 for x in A])
+
+# 807. Max Increase to Keep City Skyline
+class Solution(object): # 41.68%
+	def maxIncreaseKeepingSkyline(self, grid):
+		"""
+		:type grid: List[List[int]]
+		:rtype: int
+		"""
+		ans, h, w = 0, len(grid), len(grid[0])
+		col_sky = [max(col) for col in grid]
+		row_sky = [max([grid[i][j] for i in range(h)]) for j in range(w)]
+
+		for i in range(h):
+			for j in range(w):
+				ans += min(row_sky[i], col_sky[j]) - grid[i][j]
+
+		return ans
+
+# 938. Range Sum of BST
+# Definition for a binary tree node.
+# class TreeNode(object):
+#	 def __init__(self, x):
+#		 self.val = x
+#		 self.left = None
+#		 self.right = None
+
+class Solution(object): # 80.54%
+	def rangeSumBST(self, root, L, R):
+		"""
+		:type root: TreeNode
+		:type L: int
+		:type R: int
+		:rtype: int
+		"""
+		if not root: return 0
+		ans, stack = 0, []
+		stack.append(root)
+		while stack:
+			node = stack.pop()
+			val = node.val
+			if R >= val >= L:
+				ans += val
+				if node.left: stack.append(node.left)
+				if node.right: stack.append(node.right)
+			else:
+				if val > R and node.left: stack.append(node.left)
+				if val < L and node.right: stack.append(node.right)
+		return ans
+class Solution(object): # 80.54%
+	def rangeSumBST(self, root, L, R):
+		"""
+		:type root: TreeNode
+		:type L: int
+		:type R: int
+		:rtype: int
+		"""
+		if not root: return 0
+		if root.val > R: return self.rangeSumBST(root.left, L, R)
+		elif root.val < L: return self.rangeSumBST(root.right, L, R)
+		return root.val + self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
